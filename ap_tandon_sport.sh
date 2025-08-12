70a78ca5319715ba790821ba4841755ccfa45598
Zhodong Liu <zl4789@nyu.edu>
2025-08-12 03:16:25 -0400

    update sh

--- ap_tandon_sport.sh
+++ ap_tandon_sport.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=64GB
 #SBATCH --account=pr_119_tandon_priority
-#SBATCH --time=5:00:00
+#SBATCH --time=15:00:00
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mail-type=ALL          
 #SBATCH --mail-user=zl4789@nyu.edu
@@ -17,8 +17,8 @@ cd /scratch/zl4789/action_piece_google
 
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --category=Sports_and_Outdoors \
-    --weight_decay=0.15 \
-    --lr=0.01 \
-    --n_hash_buckets=128
+    --weight_decay=0.18 \
+    --lr=0.001 \
+    --n_hash_buckets=64
 
 conda deactivate
--- ap_tandon_sport_1.sh
+++ ap_tandon_sport_1.sh
@@ -2,10 +2,10 @@
 #SBATCH --output=jobs/Job.%j.out
 #SBATCH --error=jobs/Job.%j.err
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=64GB
 #SBATCH --account=pr_119_tandon_priority
-#SBATCH --time=40:00:00
+#SBATCH --time=15:00:00
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mail-type=ALL          
 #SBATCH --mail-user=zl4789@nyu.edu
@@ -16,11 +16,9 @@ conda activate TIGER
 cd /scratch/zl4789/action_piece_google
 
 CUDA_VISIBLE_DEVICES=0 python main.py \
-    --category=CDs_and_Vinyl \
-    --weight_decay=0.1 \
+    --category=Sports_and_Outdoors \
+    --weight_decay=0.15 \
     --lr=0.001 \
-    --d_model=256 \
-    --d_ff=2048 \
-    --n_hash_buckets=256
+    --n_hash_buckets=128
 
 conda deactivate
