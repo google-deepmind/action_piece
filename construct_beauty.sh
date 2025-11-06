e97490e5a3a0d54357deaa36d4cc99a8115e1560
Zhodong Liu <zl4789@nyu.edu>
2025-11-06 01:27:06 -0500

    update pipeline setting

---
+++ construct_beauty.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --output=jobs/Job.%j.out
+#SBATCH --error=jobs/Job.%j.err
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64GB
+#SBATCH --time=46:00:00
+#SBATCH --mail-type=ALL          
+#SBATCH --mail-user=zl4789@nyu.edu
+#SBATCH --requeue
+
+source /share/apps/anaconda3/2020.07/etc/profile.d/conda.sh;
+conda activate TIGER
+cd /scratch/zl4789/action_piece_google
+
+python build_vocab.py   \
+    --category=Beauty             \
+    --multimodal.enable=true             \
+    --multimodal.image_pca_dim=256       \
+    --multimodal.final_pca_dim=256       \
+    --dataset=AmazonReviews2018
+conda deactivate
---
+++ construct_sport.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --output=jobs/Job.%j.out
+#SBATCH --error=jobs/Job.%j.err
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64GB
+#SBATCH --time=47:00:00
+#SBATCH --mail-type=ALL          
+#SBATCH --mail-user=zl4789@nyu.edu
+#SBATCH --requeue
+
+source /share/apps/anaconda3/2020.07/etc/profile.d/conda.sh;
+conda activate TIGER
+cd /scratch/zl4789/action_piece_google
+python build_vocab.py   \
+    --category=Sports_and_Outdoors             \
+    --multimodal.enable=true             \
+    --multimodal.image_pca_dim=256       \
+    --multimodal.final_pca_dim=256       \
+    --dataset=AmazonReviews2018
+conda deactivate
